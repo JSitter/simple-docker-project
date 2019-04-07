9a63b236b5193743910f3982d348422f95b33ff4
Justin Sitter <justin@jaytria.com>
2019-04-07 15:23:44 -0700

    Heroku sets port in environment variable

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask_restplus import Api, Resource, fields
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='Simple Flask App', description='This is a flask app for flasking things and apping')
@@ -12,7 +13,9 @@ class Index(Resource):
   ''' Main page.'''
   @api.doc(parser=single_parser, description='Get main page')
   def get(self):
+    print("hello")
     return {"Who did it?": "Don't Ask"}
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000)
+  port = int(os.environ.get('PORT', 8000))
+  app.run(host='0.0.0.0', port=port)
